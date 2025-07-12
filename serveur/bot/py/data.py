b0ec8b5119728502d27d50f8c23bf6b4525d28bf
wilhelm43 <wilhelmbismarck@tutanota.de>
2025-07-12 19:17:02 +0200

    Update data.py

--- serveur/bot/py/data.py
+++ serveur/bot/py/data.py
@@ -1,5 +1,7 @@
-token  : str  = "bot_token"
-id     : int  = bot_id
-key    : str  = "bot_key"
-perm   : int  = perm_int
-filled : bool = False
+token           : str  = #bot_token
+id              : int  = #bot_id
+key             : str  = #bot_key
+perm            : int  = #discord_perm_int
+auth_project_id : int  = #scratch_auth_project_id
+scratcher              = ('username', 'password')
+filled          : bool = False # replace it with true when this file is filled
