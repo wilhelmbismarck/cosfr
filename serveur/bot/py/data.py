3e48d181e785ce9c71c86cce408d57559513d6f6
wilhelm43 <wilhelmbismarck@tutanota.de>
2025-07-13 14:03:55 +0200

    Update data.py

--- serveur/bot/py/data.py
+++ serveur/bot/py/data.py
@@ -5,3 +5,4 @@ perm            : int  = #discord_perm_int
 auth_project_id : int  = #scratch_auth_project_id
 scratcher              = ('username', 'password')
 filled          : bool = False # replace it with true when this file is filled
+guilds_id       : list = [GUILD_ID] # add your dev server ID at least
