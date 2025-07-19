b8f3ad4ad694ae4b67fa9f1e222c967383a71d80
wilhelm43 <wilhelmbismarck@tutanota.de>
2025-07-19 21:00:02 +0200

    Create session.py

---
+++ serveur/bot/py/util/sub/session.py
@@ -0,0 +1,7 @@
+token             : str  = "" #bot_token
+id                : int  = 0 #bot_id
+key               : str  = "" #bot_key
+perm              : int  = 0 #discord_perm_integer
+auth_project_id   : int  = 0 #scratch_auth_project_id
+scratcher                = ('username', 'password')
+filled            : bool = False # replace it with true when you filled this file
