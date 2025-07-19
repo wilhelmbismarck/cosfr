da3176654384050bdd4ddc06802905972591be74
wilhelm43 <wilhelmbismarck@tutanota.de>
2025-07-19 20:54:19 +0200

    Delete serveur/bot/py/data.py

--- serveur/bot/py/data.py
+++
@@ -1,8 +0,0 @@
-token           : str  = #bot_token
-id              : int  = #bot_id
-key             : str  = #bot_key
-perm            : int  = #discord_perm_int
-auth_project_id : int  = #scratch_auth_project_id
-scratcher              = ('username', 'password')
-filled          : bool = False # replace it with true when this file is filled
-guilds_id       : list = [GUILD_ID] # add your dev server ID at least
